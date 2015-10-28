8e89819d7fd8324d0b8486b458c9e51a4efe3a89
Diego A Manrique Parraga <diegomanri@gmail.com>
2015-10-28 18:51:52 -0500

    Fixed events date/time input and view, also fixed page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  # Custom page titles helper
+  def title(text)
+    content_for :title, text
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,13 @@
 class Event < ActiveRecord::Base
+  before_create :format_time
   belongs_to :creator, class_name: User
   has_many :event_registrations, foreign_key: :event_id, dependent: :destroy
   has_many :attendees, through: :event_registrations, source: :attendee, dependent: :destroy
   mount_uploader :avatar, AvatarUploader
+
+  validate :event_date_check
+  validate :event_time_check
+  validates_presence_of :edescription
 
   default_scope -> {order('edate asc')}
   scope :upcoming_events, ->  { where('edate > ?', Date.today)}
@@ -15,13 +20,13 @@ class Event < ActiveRecord::Base
   end
 
   def event_date_check
-    edate < Date.today ? errors.add(:edate, "can't be in the past") : true if !edate.nil?
+    edate < Date.today ? errors.add(:edate, "Event date can't be in the past") : true if !edate.nil?
   end
 
   def event_time_check
     if edate == Date.today && !edate.nil? && !etime.nil?
       if etime.hour < Time.now.hour
-        errors.add(:etime, "can't be in the past")
+        errors.add(:etime, "Event time can't be in the past")
       elsif etime.hour == Time.now.hour && etime.min > Time.now.min
         errors.add(:etime, "too soon to be organized")
       else
