615bda715270350307c131697aede15d9b9efc13
Diego A Manrique Parraga <diegomanri@gmail.com>
2015-11-13 12:44:30 -0600

    Added relationship rule where dogs cannot add to themselves and added .class to each of the items under relatives:

--- app/models/pedigree.rb
+++ app/models/pedigree.rb
@@ -2,5 +2,9 @@ class Pedigree < ActiveRecord::Base
   belongs_to :dog
   belongs_to :relative, class_name: Dog
 
-  validates_uniqueness_of :dog_id, :scope => :relative_id
+  #Prevents for equal dog relationships to be added more than once
+  validates :dog_id, uniqueness: {scope: :relative_id}
+
+  #Prevents for a dog to make a relationship with itself
+  validates :relative_id, exclusion: {in: -> (u){[u.dog_id]}}
 end
