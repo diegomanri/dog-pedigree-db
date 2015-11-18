4589ab6730ef8127367a42dd02299252e643748d
Diego A Manrique Parraga <diegomanri@gmail.com>
2015-11-18 17:55:50 -0600

    fixed again

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
-  before_action :confirm_logged_in, [:show, :event_search]
+  before_action :confirm_logged_in, except: [:show, :event_search]
   before_filter :admin_only, only: :index
   layout 'authenticated'#, only: [:authenticated, :dogmenu, :eventmenu, :admin]
 
